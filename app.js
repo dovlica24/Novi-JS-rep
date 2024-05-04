c8837e76b99dbe177371915790288256d350d75b
Dovlica <vladozlatovic421999@gmail.com>
2024-05-04 17:50:45 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,57 @@
+"use strict";
+
+
+console.log("Cao ja sam Vlado");
+
+
+
+window.console.log ("hehe");
+
+var mojeIme = "Vlado";
+let mojeIme2 = "Vlado";
+const mojeIme3 ="Vlado"; // komentar
+
+const mojeIme4 = "hehe";
+let mojeIme5;
+
+
+mojeIme5
+console.log(mojeIme5)
+
+mojeIme5 ="454";
+
+mojeIme5
+
+console.log (mojeIme5);
+
+mojeIme5 ="dadada"
+
+mojeIme5 = "45677";
+console.log (mojeIme5);
+
+mojeIme5 = 1923;
+console.log (mojeIme5);
+
+const broj=3123;
+
+mojeIme5 = null;
+console.log (mojeIme5);
+
+
+
+
+let brojVeciOdTri = 5 < 3;
+
+console.log (brojVeciOdTri);
+
+const podaci = {
+  ime: "Vlado",
+  godine: 25,
+  ozenjen: false,
+
+};
+
+console.log (podaci.ime);
+
+
+
